efcdd25b973c841b359a2d275269fd221feca467
slavamlinsky <slavamlinsky@gmail.com>
2023-12-23 15:59:37 +0200

    version 2.9

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const accessKey = urlParams.get("key");
 const formName = urlParams.get("form");
 
 // Script Version
-console.log("v0.2.8");
+console.log("v0.2.9");
 
 var serverUrl = "https://gecko-form-be.winno.gmbh/api/forms/submit";
 // var serverUrl = "http://localhost:5000/api/forms/submit/";
@@ -27,74 +27,38 @@ console.log("AccessKey: ", accessKey);
 console.log("FormName: ", formName);
 
 // document.addEventListener("DOMContentLoaded", function () {
+
 // Form validation handler
 const form = document.querySelector("form[name='" + formName + "']");
-// console.log(form);
+
 const formElements = form.elements;
 
 for (let i = 0; i < formElements.length; i++) {
-  // console.log(formElements[i].tagName + " - " + formElements[i].type);
   const element = formElements[i];
   const elClassName = ".cmp--" + element.classList[0];
 
   if (element.tagName === "INPUT" || element.tagName === "TEXTAREA") {
     if (element.type === "radio") {
-      // alert("RADIO");
       element.addEventListener("change", (event) => {
-        // console.log(element.value);
-        // console.log(elClassName);
-        // console.log(element.closest(elClassName));
-        // console.log(element.closest(elClassName).nextElementSibling);
-        // console.log(element.nextElementSibling);
-        // console.log(element.closest(elClassName + "-group"));
-        // console.log(element.closest(elClassName + "-group").querySelectorAll(elClassName));
         const radios = element.closest(elClassName + "-group").querySelectorAll(elClassName);
-        // console.log(radios);
 
         radios.forEach(function (el) {
           el.classList.remove("selected");
         });
 
-        // console.log(element.closest(elClassName));
-        // element.closest(elClassName).classList.remove("error");
         element.closest(elClassName).classList.add("selected");
-      });
-
-      element.addEventListener("click", (event) => {
-        // console.log("Click", element.value);
-        // console.log("Click Target", event.target.value);
-        // element.closest(elClassName).classList.remove("selected");
       });
     }
 
     if (element.type === "checkbox") {
-      alert("checkbox");
       element.addEventListener("change", (event) => {
-        // console.log(element.value);
-        // console.log(elClassName);
-        // console.log(element.closest(elClassName));
-        // console.log(element.closest(elClassName).nextElementSibling);
-        // console.log(element.nextElementSibling);
-        // console.log(element.closest(elClassName + "-group"));
-        // console.log(element.closest(elClassName + "-group").querySelectorAll(elClassName));
-        // const radios = element.closest(elClassName + "-group").querySelectorAll(elClassName);
-        // console.log(radios);
-
         console.log(event.target);
 
         if (element.checked) {
-          console.log("Checked");
+          element.closest(elClassName).classList.add("checked");
         } else {
-          console.log("UnChecked");
+          element.closest(elClassName).classList.remove("checked");
         }
-
-        // radios.forEach(function (el) {
-        //   el.classList.remove("selected");
-        // });
-
-        // console.log(element.closest(elClassName));
-        // element.closest(elClassName).classList.remove("error");
-        element.closest(elClassName).classList.add("selected");
       });
     }
 
@@ -110,10 +74,9 @@ for (let i = 0; i < formElements.length; i++) {
       element.type === "textarea"
     ) {
       if (element.disabled) {
-        //alert("Disabled");
         element.closest(elClassName).classList.add("disabled");
       }
-      // console.log(element.type);
+
       element.addEventListener("focus", (event) => {
         element.closest(elClassName).classList.remove("error");
         element.closest(elClassName).classList.add("focused");
@@ -121,7 +84,6 @@ for (let i = 0; i < formElements.length; i++) {
 
       element.addEventListener("blur", (event) => {
         element.closest(elClassName).classList.remove("focused");
-        // console.log(element.value.trim());
 
         if (element.required && element.value.trim() === "") {
           element.closest(elClassName).classList.remove("filled");
@@ -136,11 +98,9 @@ for (let i = 0; i < formElements.length; i++) {
       });
 
       element.addEventListener("input", (event) => {
-        // alert("1");
         if (element.value.trim() === "") {
           element.closest(elClassName).classList.remove("filled");
         } else {
-          // element.closest(elClassName).style.color = "red";
           element.closest(elClassName).classList.add("filled");
         }
 
@@ -152,24 +112,6 @@ for (let i = 0; i < formElements.length; i++) {
         }
       });
     }
-
-    // if (element.type === "date" || element.type === "time") {
-    //   element.addEventListener("blur", (event) => {
-    //     //alert("Blur");
-    //     if (element.value.trim() === "") {
-    //       element.closest(elClassName).classList.remove("filled");
-    //     } else {
-    //       element.closest(elClassName).classList.add("filled");
-    //     }
-
-    //     if (element.required && element.value.trim() === "") {
-    //       element.closest(elClassName).classList.add("error");
-    //       return; // Stop submission if a required field is empty
-    //     } else {
-    //       element.closest(elClassName).classList.remove("error");
-    //     }
-    //   });
-    // }
 
     if (element.type === "select-one") {
       element.addEventListener("change", (event) => {
@@ -188,20 +130,13 @@ for (let i = 0; i < formElements.length; i++) {
       });
     }
   }
-  // if (element.tagName === "TEXTAREA") {
-  //   console.log(element.type);
-  //   //alert("textArea");
-  //   if (element.disabled) {
-  //     element.closest(elClassName).classList.add("disabled");
-  //   }
-  // }
 }
 
 // Form submit handler
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  alert("Form Submit");
+  // alert("Form Submit");
 
   // Get all form elements
   const formElements = form.elements;
@@ -225,11 +160,6 @@ form.addEventListener("submit", function (e) {
         element.type === "time" ||
         element.type === "textarea"
       ) {
-        // if (element.required && element.value.trim() === "") {
-        //   element.closest(".cmp--tf").classList.add("Error");
-        //   return; // Stop submission if a required field is empty
-        // }
-
         if (element.required && element.value.trim() === "") {
           element.closest(elClassName).classList.add("error");
           return; // Stop submission if a required field is empty
@@ -239,13 +169,10 @@ form.addEventListener("submit", function (e) {
 
       // Update form data - sending all data from the form to backend point
       if (element.type !== "submit" && element.type !== "reset" && element.tagName !== "BUTTON") {
-        // console.log(element);
-        // console.log(element.nextElementSibling);
         const field = element.nextElementSibling;
         if (field) {
           labelValue = field.lastElementChild?.textContent;
         }
-        // console.log(labelValue);
 
         let newElement = {
           label: element.dataset["name"] || element.name || labelValue,
@@ -253,8 +180,6 @@ form.addEventListener("submit", function (e) {
           type: element.type,
           variable: element.dataset?.["variable"],
         };
-
-        // console.log(newElement);
 
         if (element.type === "radio") {
           if (element.checked) {
@@ -290,9 +215,6 @@ form.addEventListener("submit", function (e) {
 
   console.log("Form Submit + Data: " + JSON.stringify(requestData));
 
-  // var serverUrl = "https://gecko-form-be.winno.gmbh/api/forms/submit";
-  // var serverUrl = "http://localhost:5000/api/forms/submit/";
-
   var requestOptions = {
     method: "POST",
     headers: {
@@ -301,8 +223,6 @@ form.addEventListener("submit", function (e) {
     },
     body: JSON.stringify(requestData),
   };
-
-  // form.innerHTML = "<div class='mainForm'><h2>Sending data...</h2></div>";
 
   const submitButton = document.querySelector("input[type='submit']");
 
@@ -313,8 +233,6 @@ form.addEventListener("submit", function (e) {
   // Make the fetch request
   fetch(serverUrl, requestOptions)
     .then((response) => {
-      //console.log("Response" + response);
-      //console.log(response.json());
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
