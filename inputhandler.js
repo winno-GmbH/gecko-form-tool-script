b6721e2a3699faacc83fba8754b2e48d118fede8
slavamlinsky <slavamlinsky@gmail.com>
2024-02-16 16:07:31 +0200

    * fixed 'textarea' success on empty (v 4.12)

--- inputhandler.js
+++ inputhandler.js
@@ -1,5 +1,5 @@
 // Script Version
-console.log("Elements v0.4.11");
+console.log("Elements v0.4.12");
 
 // Inputs validation handler
 const inputs = document.querySelectorAll("input, textarea");
@@ -245,6 +245,7 @@ for (let i = 0; i < inputs.length; i++) {
       element.closest(elClassName).classList.remove("focused");
       if (element.required && element.value.trim() === "") {
         element.closest(elClassName).classList.remove("filled");
+        element.closest(elClassName).classList.remove("success");
         element.closest(elClassName).classList.add("error");
       } else if (element.value.trim() === "") {
         element.closest(elClassName).classList.remove("filled");
